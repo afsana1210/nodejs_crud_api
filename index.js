27ea1eb0abeab09fea50a148db76b20fdc2661e7
afsana1210 <afsanaansari1210@gmail.com>
2024-05-01 00:07:54 +0530

    PUT and DELETE API added

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", db.getUsers);
 app.get("/user/:id", db.getUserById);
 app.post("/user", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(port, () => {
   console.log(`App running on port:${port}`);
--- users.js
+++ users.js
@@ -40,8 +40,36 @@ const createUser = (req, res) => {
   );
 };
 
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("id is", id);
+  const { name, email } = req.body;
+  pool.query(
+    "UPDATE users SET name=$1,email=$2 WHERE id=$3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  pool.query("DELETE FROM users WHERE id=$1 ", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
+  updateUser,
+  deleteUser,
 };
